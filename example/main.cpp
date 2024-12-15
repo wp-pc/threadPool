00cc11f6935f66e047bf87e1e29600f77bc32581
wp <3197003953@qq.com>
2024-12-15 15:58:30 +0800

    changed

--- example/main.cpp
+++ example/main.cpp
@@ -1,52 +1,27 @@
-#include <iostream>
-#include <ctime>
-#include <type_traits>
 
 #include "../threadPool.hpp"
 
-void fun(int a) {
-    while(1) {
-        std::cout << "Thread id " << std::this_thread::get_id() << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+int add(int a, int b) {
+    for(;;) {
+        std::cout << a + b << std::endl;
+        ++a;
+        ++b;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
+
+    return a + b;
 }
-
-class A {
-public:
-    A() = default;
-    void operator()(){
-        for(int i=0;i<10;++i) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            std::cout << "Thread id: " << std::this_thread::get_id() << std::endl;
-        }
-    }
-};
-
-// void fun() {
-
-// }
 int main() {
-
-
-    std::cout << "Starting the proess. " << std::endl;
-    std::cout << typeid(int).name() << std::endl;
     ThreadPool pool(4);
 
-    pool.enqueue([]() {
-        while(1) {
-            std::cout << "Thread id: " << std::this_thread::get_id() << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    pool.enqueue([]{
+        for(;;) {
+            std::cout << "hello world." << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         }
+        return 1;
     });
 
-    pool.enqueue([]() {
-        while(1) {
-            std::cout << "Thread id: " << std::this_thread::get_id() << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        }
-    });
-
-    pool.enqueue(fun, 3);
-    pool.enqueue(A());
+    pool.enqueue(add, 1, 2);
 }
-
--- threadPool.hpp
+++ threadPool.hpp
@@ -1,6 +1,5 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
-
 #include <queue>
 #include <vector>
 #include <mutex>
@@ -8,7 +7,7 @@
 #include <functional>
 #include <thread>
 #include <iostream>
-#include <future>
+
 class ThreadPool
 {
     using Task = std::function<void()>;
@@ -16,12 +15,8 @@ class ThreadPool
 public:
     explicit ThreadPool(size_t threads);
 
-    // add task for work_queue
     template <class F, class... Args>
-    auto enqueue(F &&f, Args &&...args)->void;
-        // std::future<typename std::result_of<F(Args...)>::type>;
-
-    // m_stop the thread_pool
+    void enqueue(F&& f, Args&& ...args);
     void stop();
     ~ThreadPool();
 
@@ -30,65 +25,39 @@ private:
     std::queue<Task> m_tasks;
     std::mutex m_queueMutex;
     std::condition_variable m_cv;
-    std::thread m_mangerThead;
-    bool m_stop;
 
+    bool m_stop;
 private:
-    void executeManger();
+    void executeTask();
 };
 
+
+ThreadPool::ThreadPool(size_t threads)
+    : m_stop(false)
+{
+    for (int i = 0; i < threads; ++i)
+    {
+        m_workers.emplace_back(&ThreadPool::executeTask,this);
+    }
+}
+
+// add task for work_queue
 template <class F, class... Args>
-auto ThreadPool::enqueue(F &&f, Args &&...args)->void
-    // std::future<typename std::result_of<F(Args...)>::type>
+auto ThreadPool::enqueue(F&& f, Args&& ...args) -> void // std::future<typename std::result_of<F(Args...)>::type>
 {
     auto task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+
     std::unique_lock<std::mutex> lock{m_queueMutex};
     if (m_stop)
     {
         throw std::runtime_error("the threadPool is stopped.");
-        // throw std::runtime_error("the threadPool is stopped. ");
     }
     m_tasks.emplace(task);
     lock.unlock();
     m_cv.notify_one();
 }
 
-ThreadPool::ThreadPool(size_t threads)
-    : m_stop(false)
-{
-
-    // 初始化管理线程
-    m_mangerThead = std::thread([this]
-                                {
-        for(int i=0;i<20;++i) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            stop();
-        } });
-    m_mangerThead.detach();
-    for (int i = 0; i < threads; ++i)
-    {
-        m_workers.emplace_back([this]()
-                               {
-            while(true) {
-                std::unique_lock<std::mutex> lock{m_queueMutex};
-                m_cv.wait(lock, [this](){
-                    if(m_stop || !(m_tasks.empty()))
-                        return true;
-                    else
-                        return false; 
-                });
-
-                if(m_stop && m_tasks.empty())
-                    return ;
-
-                auto task = std::move(m_tasks.front());
-                m_tasks.pop();
-                lock.unlock();
-                task();
-            } });
-    }
-}
-
+// m_stop the thread_pool
 inline void ThreadPool::stop()
 {
     std::unique_lock<std::mutex> lock(m_queueMutex);
@@ -101,8 +70,29 @@ inline void ThreadPool::stop()
     }
 }
 
+// exectuTask
+void ThreadPool::executeTask()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> lock{m_queueMutex};
+        m_cv.wait(lock,[this]{
+            return m_stop || !(m_tasks.empty());
+        });
+        if (m_stop && m_tasks.empty())
+            return;
+
+        auto task = std::move(m_tasks.front());
+        m_tasks.pop();
+
+        lock.unlock();
+        task();
+    }
+}
+
 ThreadPool::~ThreadPool()
 {
     stop();
 }
+
 #endif
